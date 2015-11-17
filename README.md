fc53a2f67ec8396a4050ef25c0fa44b1b5a67a3f
Lucas Videla <videla.lucas@gmail.com>
2015-11-16 21:34:17 -0300

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # melhores-filtros
-:plane: Mejores filtros para la web melhoresdestinos.com.br
+
+Una extensión para [Chrome][ext] que mejora la usabilidad de los filtros de la web de búsqueda de vuelos y ofertas [www.mejoresdestinos.com.ar][md].
+
+
+[ext]: https://chrome.google.com/webstore/detail/melhores-filtros-para-mel/ccaeldicckaopbdpmdhhkpmampnplaje
+[md]: www.mejoresdestinos.com.ar
---
+++ filtros.js
@@ -0,0 +1,66 @@
+// Helpers
+ Date.prototype.weFormat = function() {
+   var yyyy = this.getFullYear().toString();
+   var mm = (this.getMonth()+1).toString();
+   var dd  = this.getDate().toString();
+   return yyyy + '-' + (mm[1]?mm:"0"+mm[0]) + '-' + (dd[1]?dd:"0"+dd[0]);
+ };
+
+// Aquí comienza la intervención
+
+ function fiddle() {
+
+    if (!$("body").hasClass("geekviajero")) {
+
+      // Calculamos las estadias y cambiamos los formatos de fecha
+      $(".tbtrechos tr").each(function(index, item) {
+
+        if (index == 0) return;
+
+        var price = parseInt($(item).find(".pc").text().split(" ")[1].replace('.',''));
+        var priceCell = $(item).find(".pc");
+        priceCell.html(priceCell.text() + "<br/>USD " + price/4);
+
+
+        var sale = $($(item).find('.dt')[0]).text().split('/');
+        var llega = $($(item).find('.dt')[1]).text().split('/');
+        var thisMonth = (new Date()).getMonth()+1;
+        var thisYear = (new Date()).getFullYear();
+        var fechaSale = new Date( (parseInt(sale[1])>=thisMonth ? thisYear + '/' : (thisYear+1) + '/') + sale[1] + '/' + sale[0]);
+        $($(item).find('.dt')[0]).text(fechaSale.weFormat());
+        var fechaLlega = new Date( (parseInt(llega[1])>=thisMonth ? thisYear + '/' : (thisYear+1) + '/') + llega[1] + '/' + llega[0]);
+        $($(item).find('.dt')[1]).text(fechaLlega.weFormat());
+
+        var estadia = parseInt((fechaLlega - fechaSale) / (1000 * 60 * 60 * 24));
+
+        var toCell = $($(item).find(".dt")[1]);
+        toCell.html(toCell.text() + "<br/>" + estadia + " dias");
+
+
+      });
+
+      // Removemos filtros de la página
+      $(".filtros_tabela").detach();
+
+      // Rearmamos la tabla para que sea DataTables-friendly
+      headers = $(".tbtrechos tr").first();
+      headers.parent().parent().prepend("<thead>");
+      headers.parent().parent().find("thead").append(headers);
+
+      // Agregamos CSS y JS de DataTables, e invocamos
+        $("head").append(
+          $("<link>")
+          .attr('rel', 'stylesheet')
+          .attr('type', 'text/css')
+          .attr('href', 'https://cdn.datatables.net/1.10.10/css/jquery.dataTables.min.css')
+          )
+         $('.tbtrechos').dataTable();
+
+        // Marcamos el body, para no ejecutar dos veces
+      $("body").addClass("geekviajero");
+
+    }
+
+ }
+   fiddle();
+
